5b303237b688139702b0e50a38be41208c2af374
swyssauro <xspynni23@gmail.com>
2021-04-12 13:37:46 -0300

    �‍� fresh update users.map

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,27 +1,23 @@
 import { GetStaticProps } from "next";
-import Data from "../interfaces/data";
+import Data from '../interfaces/data';
 
-
-export default function Friends({ users }) {
-    return(
-      <div>
-    {users.map(user => {
-      console.log(user.avatarURL);
-          <img src={user.avatar.startsWith('a_') === true ? user.avatarURL.replace('webp', 'gif') : user.avatarURL}/>
-      })}
-      </div>
-      
-    )
+export default function Home({ users }) {
+  return (
+    <div className="container">
+      {users.map(friend => (
+        <img src={friend.avatar.startsWith('a_') === true ? friend.avatarURL.replace('webp', 'gif') : friend.avatarURL} alt="such"/>
+      ))}
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const response = await fetch("http://localhost:4444");
-    const data: Data = await response.json();
-    return {
-      props: {
-        users: data,
-      },
-      revalidate: 5,
-    };
+  const response = await fetch("http://localhost:4444");
+  const data: Data = await response.json();
+  return {
+    props: {
+      users: data,
+    },
+    revalidate: 5,
   };
-  
+};
